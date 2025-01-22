2d4172b04358a51ddd01f0c3f202fb991051ccfb
Ibtissam COSSON <ichaari@gmail.com>
2025-01-23 00:38:34 +0100

    intégration des test et description des tables

--- quete_dbt_intro/models/staging/stg_orders.sql
+++ quete_dbt_intro/models/staging/stg_orders.sql
@@ -4,7 +4,9 @@ with source as (
 renamed as (
     select
         id as customer_id,
-        customer as customer_name
+        customer as customer_name,
+        ordered_at as ordered_at,
+        store_id as store_id
     from source
 )
 select * from renamed
